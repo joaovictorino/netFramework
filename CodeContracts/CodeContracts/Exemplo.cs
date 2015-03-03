fc1a4d78c337bbc0533110cb7584738b031698f1
joaovictorino <jhvictorino@gmail.com>
2015-03-03 19:12:16 -0300

    Teste de code contracts

---
+++ CodeContracts/CodeContracts/Exemplo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContracts
+{
+    public class Exemplo
+    {
+        public void Initialize(string name, int id)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(name));
+            Contract.Requires(id > 0);
+
+            Contract.Ensures(Name == name);
+            Contract.Ensures(Id == id);
+        }
+
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
