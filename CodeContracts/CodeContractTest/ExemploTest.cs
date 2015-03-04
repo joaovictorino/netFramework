3fbe147807914bd7faf26ba5c5f387724ea82844
joaovictorino <jhvictorino@gmail.com>
2015-03-04 12:36:38 -0300

    Classe de teste unitário

---
+++ CodeContracts/CodeContractTest/ExemploTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeContracts;
+
+namespace CodeContractTest
+{
+    [TestClass]
+    public class ExemploTest
+    {
+        [TestMethod]
+        public void TestarEnsures()
+        {
+            Exemplo ex = new Exemplo();
+            ex.Name = "teste";
+            ex.Id = 1;
+            ex.Initialize("teste", 1);
+        }
+    }
+}
