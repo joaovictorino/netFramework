d4c3ccef22305304a3d89fd6e021e9c8d4dfe259
joaovictorino <jhvictorino@gmail.com>
2015-03-05 18:37:55 -0300

    Reestruturando exemplos

---
+++ FSharpTest/RabbitMQTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+using RabbitMQ.Client.Events;
+using System.Text;
+using RabbitMQ.Client.MessagePatterns;
+
+namespace RabbitMQTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = "192.168.56.101";
+            connectionFactory.Port = 5672;
+            connectionFactory.UserName = "joao";
+            connectionFactory.Password = "joao";
+            connectionFactory.VirtualHost = "/";
+
+            using (IConnection connection =
+                        connectionFactory.CreateConnection())
+            {
+                using (IModel model = connection.CreateModel())
+                {
+                    //criar fila
+                    model.ExchangeDeclare("MyExchange", ExchangeType.Fanout, true);
+                    model.QueueDeclare("MyQueue", true, false, false, new Dictionary<string, object>());
+                    model.QueueBind("MyQueue", "MyExchange", "", new Dictionary<string, object>());
+                    string message = "Hello!!";
+
+                    //colocar mensagem
+                    IBasicProperties basicProperties = model.CreateBasicProperties();
+                    model.BasicPublish("MyExchange", "", false, false,
+                        basicProperties, Encoding.UTF8.GetBytes(message));
+
+                    //puxar mensagem
+                    var subscription = new Subscription(model, "MyQueue", false);
+                    while (true)
+                    {
+                        BasicDeliverEventArgs basicDeliveryEventArgs =
+                            subscription.Next();
+                        string messageContent =
+                            Encoding.UTF8.GetString(basicDeliveryEventArgs.Body);
+                        Console.WriteLine(messageContent);
+                        subscription.Ack(basicDeliveryEventArgs);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FSharpTest/RiakTest/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CorrugatedIron;
+using CorrugatedIron.Models;
+using CorrugatedIron.Models.Search;
+using System.Collections.Generic;
+using System.Net;
+using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace RiakTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            #region riak
+            var cluster = RiakCluster.FromConfig("riakConfig");
+            var client = cluster.CreateClient();
+
+            var p = new Person()
+            {
+                EmailAddress = "oj@buffered.io",
+                FirstName = "OJ",
+                LastName = "Reeves"
+            };
+
+            var o = new RiakObject("contributors", p.EmailAddress, p);
+
+            client.Put(o);
+
+            var search = new RiakFluentSearch("contributors", "firstname").Search(Token.StartsWith("OJ")).Build();
+            RiakResult<IEnumerable<string>> resultado = (RiakResult<IEnumerable<string>>)client.ListBuckets();
+
+            var resultado2 = client.Get("contributors", "oj@buffered.io");
+
+            if (resultado2.IsSuccess)
+            {
+               var person = resultado2.Value.GetObject<Person>();
+            }
+
+            string URI = "https://localhost/TestePost/About";
+
+            using (WebClient wc = new WebClient())
+            {
+                NameValueCollection values = new NameValueCollection();
+                values.Add("teste","teste1");
+                byte[] data = wc.UploadValues(URI, "POST", values);
+                string resultado3 = System.Text.Encoding.UTF8.GetString(data);
+            }
+
+            Uri address = new Uri("https://localhost/TestePost/About");
+
+            ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+
+            using (WebClient webClient = new WebClient())
+            {
+                //var stream = webClient.OpenRead(address);
+                //using (StreamReader sr = new StreamReader(stream))
+                //{
+                //    var page = sr.ReadToEnd();
+                //}
+
+                NameValueCollection values = new NameValueCollection();
+                values.Add("teste", "teste1");
+                byte[] data = webClient.UploadValues(address, "POST", values);
+                string resultado4 = System.Text.Encoding.UTF8.GetString(data);
+            }
+            #endregion
+        }
+
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors error)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (error == System.Net.Security.SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            Console.WriteLine("X509Certificate [{0}] Policy Error: '{1}'",
+                cert.Subject,
+                error.ToString());
+
+            return true;
+        }
+    }
+}
